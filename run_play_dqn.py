768856545e71a33ed88b61439bfdae59110fe7c0
yunycu <100680150+yunycu@users.noreply.github.com>
2022-06-14 16:36:41 +0800

    Add files via upload

---
+++ run_play_dqn.py
@@ -0,0 +1,40 @@
+import gym
+import pygame
+import gym_tetris
+import numpy as np
+
+from gym_tetris.ai.DQN import DQN
+
+
+def main():
+    env = gym.make("tetris-v1", action_mode=1)
+    network = DQN(gamma=1, epsilon=0, epsilon_min=0, epsilon_decay=0)
+    network.load()
+
+    obs = env.reset()
+    running = True
+    display = True
+
+    rewards = []
+    while running:
+        action, state = network.choose_action(obs)
+        obs, reward, done, info = env.step(action)
+        rewards.append(reward)
+        if display:
+            env.render()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    display = not display
+        if done:
+            obs = env.reset()
+            print(f"reward: {np.mean(rewards)}")
+
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ run_train_dqn.py
@@ -0,0 +1,61 @@
+import gym
+import gym_tetris
+import os
+import numpy as np
+
+from pathlib import Path
+from statistics import mean, median
+from gym_tetris.ai.DQN import DQN
+
+def load_reward():
+    if not os.path.exists("./Rewards"):
+	    os.mkdir("./Rewards")
+    else:
+        if Path("./Rewards/DQN_rewards.npy").is_file():
+            return np.load("./Rewards/DQN_rewards.npy").tolist()
+        else:
+            return []
+
+
+def main():
+    env = gym.make("tetris-v1", action_mode=1)
+    network = DQN() 
+    network.load()
+
+    SEED = 50
+    env.seed(SEED)
+    network.seed(SEED)
+    env.action_space.seed(SEED)
+    
+    total_rewards = load_reward()
+
+    running = True
+    total_games = 0
+    total_steps = 0
+    
+    while total_games < 5000:
+        steps, rewards, scores = network.train(env, episodes=25)
+        total_rewards.append(rewards)
+        
+        total_games += len(scores)
+        total_steps += steps
+        network.save()
+        print("==================")
+        print("* Total Games: ", total_games)
+        print("* Total Steps: ", total_steps)
+        print("* Epsilon: ", network.epsilon)
+        print("*")
+        print("* Average: ", sum(rewards) / len(rewards), "/", sum(scores) / len(scores))
+        print("* Median: ", median(rewards), "/", median(scores))
+        print("* Mean: ", mean(rewards), "/", mean(scores))
+        print("* Min: ", min(rewards), "/", min(scores))
+        print("* Max: ", max(rewards), "/", max(scores))
+        print("==================")
+
+    np.save("./Rewards/DQN_rewards.npy", np.array(total_rewards))
+
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
